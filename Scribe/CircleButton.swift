e78ae62f839d1dd8ae535ba0c715660992fcba20
Jeff Ripke <jripke74@gmail.com>
2017-05-31 07:17:17 -0700

    built main.storyboard

---
+++ Scribe/CircleButton.swift
@@ -0,0 +1,21 @@
+//
+//  CircleButton.swift
+//  Scribe
+//
+//  Created by Jeff Ripke on 5/31/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
